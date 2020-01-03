28615c43d752414dc34f66696999dd192a7d509d
HJ-SEO <59248817+HJ-SEO@users.noreply.github.com>
2020-01-03 16:41:47 +0900

    Add files via upload

---
+++ A+B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int A, B;
+	cout << "Let me calculate A+B." << endl;
+	cout << "Enter the A: ";
+	cin >> A;
+	cout << "Enter the B: ";
+	cin >> B;
+	cout << "A+B is " << A+B << "." << endl;
+	return 0;
+}
